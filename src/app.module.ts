fc0421d44e83b1663b9c25c230f4d6d7e656e252
Ibrahim Bakhsh <igbmadinah@gmail.com>
2023-11-05 00:00:21 +0300

    log monitor

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,19 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { ArticlesModule } from './articles/articles.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { LoggerMiddleware } from './middlewares/logger.middleware';
 
 @Module({
   imports: [PrismaModule, ArticlesModule, UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
---
+++ src/middlewares/logger.middleware.ts
@@ -0,0 +1,40 @@
+import { Injectable, NestMiddleware, Logger } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware {
+  private logger = new Logger('Nest-API');
+
+  use(request: Request, response: Response, next: NextFunction): void {
+    const { ip, method, originalUrl } = request;
+    const userAgent = request.get('user-agent') || '';
+    const requestLength = +request.get('content-length');
+    let requestBody = request.body;
+    if (requestLength > 500) {
+      requestBody = request.body.substring(0, 500);
+    }
+
+    response.on('finish', () => {
+      const { statusCode } = response;
+      const contentLength = response.get('content-length');
+
+      this.logger.log(`request:`, {
+        headers: request.headers,
+        body: requestBody,
+        original_url: originalUrl,
+        user_agent: userAgent,
+        ip: ip,
+        request_method: method,
+        request_length: requestLength,
+        response_code: statusCode,
+        response_length: contentLength,
+      });
+
+      // this.logger.log(
+      //   `${method} ${originalUrl} ${statusCode} ${contentLength} - ${userAgent} ${ip}`,
+      // , response);
+    });
+
+    next();
+  }
+}
