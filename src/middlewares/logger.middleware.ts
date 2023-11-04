6d0205b928f3bb47a08c3552217265e9d9db5f4e
Ibrahim Bakhsh <igbmadinah@gmail.com>
2023-11-05 01:37:29 +0300

    disable logging in this branch

--- src/middlewares/logger.middleware.ts
+++ src/middlewares/logger.middleware.ts
@@ -18,17 +18,17 @@ export class LoggerMiddleware implements NestMiddleware {
       const { statusCode } = response;
       const contentLength = response.get('content-length');
 
-      this.logger.log(`request:`, {
-        headers: request.headers,
-        body: requestBody,
-        original_url: originalUrl,
-        user_agent: userAgent,
-        ip: ip,
-        request_method: method,
-        request_length: requestLength,
-        response_code: statusCode,
-        response_length: contentLength,
-      });
+      // this.logger.log(`request:`, {
+      //   headers: request.headers,
+      //   body: requestBody,
+      //   original_url: originalUrl,
+      //   user_agent: userAgent,
+      //   ip: ip,
+      //   request_method: method,
+      //   request_length: requestLength,
+      //   response_code: statusCode,
+      //   response_length: contentLength,
+      // });
 
       // this.logger.log(
       //   `${method} ${originalUrl} ${statusCode} ${contentLength} - ${userAgent} ${ip}`,
